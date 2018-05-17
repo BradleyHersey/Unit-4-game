258aec49e122e5669106528dbb4ad2813d74b884
Brad <bradleyhersey00@gmail.com>
2018-05-16 21:47:33 -0400

    Done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,13 @@
 $(document).ready(function () {
-
     var random = Math.floor(Math.random() * 50 + 19);//makes a randon number 
-    console.log(random);
+   
     $('#randomNumber').text(random);//shows random number 
 
     var playerTotal = 0,
         wins1 = 0,
         losses1 = 0,
-        
-       gem1 = Math.floor(Math.random() * 5 + 1),
+
+        gem1 = Math.floor(Math.random() * 5 + 1),
         gem2 = Math.floor(Math.random() * 9 + 1),
         gem3 = Math.floor(Math.random() * 10 + 1),
         gem4 = Math.floor(Math.random() * 4 + 2);//random numbers lol dont know thats its going to show =)
@@ -32,7 +31,7 @@ $(document).ready(function () {
         wins1++;
         $('#wins').text(wins1);
         reset();
-        alert('yay');
+        
     }// adds to the winner
     console.log(winner);
     function loser() {
@@ -41,41 +40,48 @@ $(document).ready(function () {
         reset();
     }//add to the loser lol
     console.log(loser);
-    $("#one").on('click',function(){
-        playerTotal=playerTotal+gem1;
+    $("#one").on('click', function () {
+        playerTotal = playerTotal + gem1; 
+       document.getElementById('pilots').play();
         $('#finalScore').text(playerTotal);
-        if(playerTotal==random){
+        if (playerTotal == random) {
             winner();
-        }else if(playerTotal>random){
+        } else if (playerTotal > random) {
+            loser();
+        }
+       
+    })
+    $("#two").on('click', function () {
+        playerTotal = playerTotal + gem2;
+        $('#finalScore').text(playerTotal);
+        if (playerTotal == random) {
+            winner();
+        } else if (playerTotal > random) {
             loser();
         }
     })
-    $("#two").on('click',function(){
-        playerTotal=playerTotal+gem2;
+    $("#three").on('click', function () {
+        playerTotal = playerTotal + gem3;
         $('#finalScore').text(playerTotal);
-        if(playerTotal==random){
+        if (playerTotal == random) {
             winner();
-        }else if(playerTotal>random){
+        } else if (playerTotal > random) {
             loser();
         }
     })
-    $("#three").on('click',function(){
-        playerTotal=playerTotal+gem3;
+    $("#four").on('click', function () {
+        playerTotal = playerTotal + gem4;
         $('#finalScore').text(playerTotal);
-        if(playerTotal==random){
+        if (playerTotal == random) {
             winner();
-        }else if(playerTotal>random){
+        } else if (playerTotal > random) {
             loser();
         }
     })
-    $("#four").on('click',function(){
-        playerTotal=playerTotal+gem4;
-        $('#finalScore').text(playerTotal);
-        if(playerTotal==random){
-            winner();
-        }else if(playerTotal>random){
-            loser();
-        }
+    $('#resetBtn').on('click',function(){
+        $('#wins').text(0);
+        $('#losses').text(0);
+        reset();
     })
 
 
